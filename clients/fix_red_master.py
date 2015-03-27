911a141e208eed8f045f67100bb71dde48ea858a
ross hutson <rosshutson@gmail.com>
2015-03-27 15:24:48 -0600

    fix fix red master

--- clients/fix_red_master.py
+++ clients/fix_red_master.py
@@ -1,6 +1,6 @@
 import labrad
 cxn = labrad.connect()
 n = cxn.node_yesr12
-n.stop('yesr12_red_master_dds')
-n.restart('yesr12_serial_server')
-n.start('red_master_dds')
+n.stop('yesr12 Red Master DDS')
+n.restart('yesr12 Serial Server')
+n.start('Red Msater DDS')
