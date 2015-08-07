a84192b729cf19b86b99b5fcef67d95cf0d7bc58
ross hutson <rosshutson@gmail.com>
2015-08-06 20:09:38 -0600

    .

--- clients/sequencer_control/sequencer_control.py
+++ clients/sequencer_control/sequencer_control.py
@@ -1150,6 +1150,9 @@ class Sequencer(QtGui.QWidget):
             if c.key() == QtCore.Qt.Key_S:
                 self.save_sequence()
                 self.run_sequence(c)
+
+    def closeEvent(self, x):
+        self.reactor.stop()
         
 digital_channels = {
                     'A00': {'name': '3D MOT AOM', 'mode': 'auto', 'manual state': 0, 'invert': 1},
