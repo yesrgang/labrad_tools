5618f8e6fc9578af42ccfedd31aae21e2edea2bd
yesrgang <yesrgang@gmail.com>
2015-10-11 20:05:28 -0600

    .

--- okfpga/sequencer/analog_config.py
+++ okfpga/sequencer/analog_config.py
@@ -1,53 +1,64 @@
 import numpy as np
 
+class DACBoard(object):
+    def __init__(self, **kwargs):
+        """ defaults """
+	self.bit_file = 'dac_glitchfix2.bit'
+	self.mode_nums = {'idle': 0, 'load': 1, 'run': 2}
+	self.mode = 'idle'
+	self.mode_wire = 0x00
+	self.channel_mode_wire = 0x09
+	self.manual_voltage_wires = {'00': 0x01, '01': 0x02, '02': 0x03, '03': 0x04, 
+                                     '04': 0x05, '05': 0x06, '06': 0x07, '07': 0x08}
+        self.clk_frequency = 48e6 / (8.*2. + 2.)
+        """ non-defaults """
+        for kw in kwargs:
+            setattr(self, kw, kwargs[kw])
+
+	self.channels = {c.key: c for c in self.channels}
+        
+	self.name_to_key = {d['name']: k for k, d in self.channels.items()}
+
+class DACChannel(object):
+    def __init__(self, **kwargs):
+        for kw in kwargs:
+            setattr(self, kw, kwargs[kw])
+	self.key = name+'@'+loc
+
 class SequencerConfig(object):
     def __init__(self):
         self.update_id = 698023
-        self.okDeviceID = 'sr2 dac1'
-        self.bit_file = 'dac_glitchfix2.bit'
-        self.sequencer_mode_num = {'idle': 0, 'load': 1, 'run': 2}
-        self.sequencer_mode = 'idle'
-	self.channel_mode_wire = 0x09
-        self.manual_voltage_wires = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]
-        self.clk_frequency = 48e6 / (8.*2. + 2.)
-        self.ramps = {
-                     'linear': linear_ramp,
-                     'exp': exp_ramp,
-                     }
-
-        """
-        channels is a dictionary for all channels.
-        keys get mapped to physical channels through sorted(). should be kept same as labels on the sequencer box.
-        the dictionary returns another dictionary with configuration parameters:
-            'name' can be any string
-            'mode' is 'auto' or 'manual'
-            'manual voltage' is number in Volts [-10, 10]
-        """
-        self.channels = {
-                        'E00': {'name': 'Alpha Intensity', 'mode': 'auto', 'manual voltage': 0},
-                        'E01': {'name': 'Beta Intensity', 'mode': 'auto', 'manual voltage': 0},
-                        'E02': {'name': 'X Comp. Coil', 'mode': 'auto', 'manual voltage': 0},
-                        'E03': {'name': 'Y Comp. Coil', 'mode': 'auto', 'manual voltage': 0},
-                        'E04': {'name': 'Z Comp. Coil', 'mode': 'auto', 'manual voltage': 0},
-                        'E05': {'name': 'MOT Coil', 'mode': 'auto', 'manual voltage': 0},
-                        'E06': {'name': 'DACE06', 'mode': 'manual', 'manual voltage': -10},
-                        'E07': {'name': 'DACE07', 'mode': 'auto', 'manual voltage': -10},
-                        }
-
-        self.name_to_key = {d['name']: k for k, d in self.channels.items()}
-
-def linear_ramp(p):
-    return [p['t']], [p['vf']]
-
-def exp_ramp(p):
-    a = (p['vf'] - p['vi']) / (np.exp(p['t']/p['tau']) - 1)
-    c = p['vi'] - a
-    continuous = lambda t: a * np.exp(t/p['tau']) + c
-    T = np.linspace(0, p['t'], p['pts']+1)
-    dT = [self.p['t']/float(p['pts'])]*p['pts']
-    V = continuous(T)
-    V[-1] = p['vf']
-    return dT, V[1:]
-
-
-
+	self.mode = 'idle'
+        self.boards = {
+            'E': DACBoard(
+                device_id='sr2 dac1',
+                channels=[
+                    DACChannel(loc='E00', name='Alpha Intensity', mode='auto', manual_voltage=0),
+                    DACChannel(loc='E01', name='Beta Intensity', mode='auto', manual_voltage=0),
+                    DACChannel(loc='E02', name='X Comp. Coil', mode='auto', manual_voltage=0),
+                    DACChannel(loc='E03', name='Y Comp. Coil', mode='auto', manual_voltage=0),
+                    DACChannel(loc='E04', name='Z Comp. Coil', mode='auto', manual_voltage=0),
+                    DACChannel(loc='E05', name='MOT Coil', mode='auto', manual_voltage=0),
+                    DACChannel(loc='E06', name='HODT', mode='auto', manual_voltage=-10),
+                    DACChannel(loc='E07', name='dimple', mode='auto', manual_voltage=-10),
+		    ],
+                 ),
+            'D': DACBoard(
+                device_id='srq analog 2',
+                channels=[
+                    DACChannel(loc='D00', name='DACD00', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D01', name='DACD01', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D02', name='DACD02', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D03', name='DACD03', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D04', name='DACD04', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D05', name='DACD05', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D06', name='DACD06', mode='auto', manual_voltage=0),
+                    DACChannel(loc='D07', name='DACD07', mode='auto', manual_voltage=0),
+		    ],
+                ),
+            }
+	
+#	self.channels = {}
+#	for b in self.boards:
+#            for k, c in b.channels.items():
+#                self.channels[k] = c
--- okfpga/sequencer/analog_sequencer.py
+++ okfpga/sequencer/analog_sequencer.py
@@ -15,17 +15,19 @@ message = 987654321
 timeout = 20
 ### END NODE INFO
 """
-
-from labrad.server import LabradServer, setting, Signal
-from twisted.internet import reactor
-from twisted.internet.defer import inlineCallbacks, returnValue
-from twisted.internet.threads import deferToThread
 import json
 import time
 import numpy as np
 import ok
 
-class SequencerServer(LabradServer):
+from labrad.server import LabradServer, setting, Signal
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.threads import deferToThread
+
+from analog_errors import *
+
+class AnalogSequencerServer(LabradServer):
     name = '%LABRADNODE% Analog Sequencer'
     mode='idle'
 
@@ -36,24 +38,18 @@ class SequencerServer(LabradServer):
         self.update = Signal(self.update_id, 'signal: update', 's')
 
     def load_configuration(self):
-        config = __import__(self.config_name).SequencerConfig()
+        config = __import__(self.config_name).AnalogSequencerConfig()
         for key, value in config.__dict__.items():
             setattr(self, key, value)
 
     def initServer(self):
-        self.initialize_board()
-        self.write_channel_modes()
-        for d in self.channels.values():
-            self.write_channel_manual_voltage(d['name']) 
+        for board in self.boards:
+            success = self.initialize_board(board)
+	        if not success:
+                self.boards.pop(board)
+        self.initialize_outputs()
 
-    def initialize_board(self):
-        connected = self.connect_board()
-        if not connected:
-            print "sequencer board not found"
-#            raise Exception("sequencer board not found")
-        self.program_board()
-
-    def connect_board(self):
+    def initialize_board(self, board):
         fp = ok.FrontPanel()
         module_count = fp.GetDeviceCount()
         print "Found {} unused modules".format(module_count)
@@ -62,34 +58,50 @@ class SequencerServer(LabradServer):
             tmp = ok.FrontPanel()
             tmp.OpenBySerial(serial)
             iden = tmp.GetDeviceID()
-            if iden == self.okDeviceID:
-                self.xem = tmp
+            if iden == board.device_id:
+                board.xem = tmp
                 print 'Connected to {}'.format(iden)
-                self.program_board()
+                board.xem.LoadDefaultPLLConfiguration() 
+                prog = board.xem.ConfigureFPGA(board.bit_file)
+                if prog:
+                   print "unable to program sequencer"
+				   return False
                 return True
         return False
 
-    def program_board(self):
-        self.xem.LoadDefaultPLLConfiguration() 
-        prog = self.xem.ConfigureFPGA(self.bit_file)
-        if prog:
-           print "unable to program sequencer"
+    def initialize_outputs(self):
+        for b in self.boards:
+            self.write_channel_modes(b)
+            for c in b.channels:
+                self.write_manual_voltage(c)
 
+    def id2channel(self, channel_id):
+        """
+        expect 3 possibilities for channel_id.
+        1) name -> return channel with that name
+        2) @loc -> return channel at that location
+        3) name@loc -> first try name, then location
+        """
+        channel = None
+        try:
+            name, loc = channel_id.split('@')
+        except:
+            name = channel_id
+            loc = None
+        if name:
+            for b in self.boards.values():
+                for c in b.channels.values():
+                    if c.name == name:
+                        channel = c
+        if not channel:
+            for b in self.boards.values():
+                for c in b.channels.values():
+                    if c.loc == loc:
+                        channel = c
+        return channel
 
-    def time_to_ticks(self, time):
-        return int(time*self.clk_frequency)
-    
-#    def _voltages_to_ramp_rates(self, times, voltages):
-#    vi = np.array([0] + voltages)
-#    vf = np.array(voltages + [voltages[-1]])
-#    voltage_diffs = vf[:-1] - vi[:-1]
-#    ramp_rates = [int(voltage_to_unsigned(v)*2.**int(np.log2(time_to_ticks(t)))/(time_to_ticks(t))) for v, t in zip(voltage_diffs, times)]
-#    for i,r in enumerate(ramp_rates):
-#        if r > 0:
-#            ramp_rates[i] = r +1
-#        else:
-#            ramp_rates[i] = 2**16 + r 
-#    return ramp_rates
+    def time_to_ticks(self, board, time):
+        return int(board.clk_frequency*time)
 
     def voltage_to_signed(self, voltage):
         voltage = sorted([-20, voltage, 20])[1]
@@ -108,63 +120,30 @@ class SequencerServer(LabradServer):
         else:
             return signed_ramp_rate + 2**16
 
-#    def _make_sequence(self, sequence):
-#        """ sequence is list of tuples [(duration, {name: logic}), ...] """
-#        # remove unnecessary ramps -> get all the ramp rates. if consequtive ramprates are same, combine!!!
-#        # write necessary ramps in correct order. Q
-#        ba = []
-#        for k, d in sorted(self.channels.items()):
-#            dv = sequence[0][1][d['name']]['v'] 
-#            if dv == 0:
-#                t = sequence[0][0]
-#                n = 0
-#                while sequence[n+1][1][d['name']]['v'] == sequence[n][1][d['name']]['v']:
-#                    t += sequence[n+1][0]
-#                    if n < len(sequence)-2:
-#                        n += 1
-#                    else:
-#                        break
-#                ba += [int(eval(hex(self.ramp_rate(dv, t))) >> i & 0xff) for i in range(0, 16, 8)]
-#                ba += [int(eval(hex(self.time_to_ticks(t))) >> i & 0xff) for i in range(0, 32, 8)]
-#            else:
-#                t = sequence[0][0]
-#                ba += [int(eval(hex(self.ramp_rate(dv, t))) >> i & 0xff) for i in range(0, 16, 8)]
-#                ba += [int(eval(hex(self.time_to_ticks(t))) >> i & 0xff) for i in range(0, 32, 8)]
-#        for m in range(1, len(sequence)):
-#            for k, d in sorted(self.channels.items()):
-#                if float(sequence[m][1][d['name']]['v']) != float(sequence[m-1][1][d['name']]['v']): # we need to change voltage
-#                    dv = sequence[m][1][d['name']]['v'] - sequence[m-1][1][d['name']]['v']
-#                    t = sequence[m][0]
-#                    ba += [int(eval(hex(self.ramp_rate(dv, t))) >> i & 0xff) for i in range(0, 16, 8)]
-#                    ba += [int(eval(hex(self.time_to_ticks(t))) >> i & 0xff) for i in range(0, 32, 8)]
-#                elif sequence[m-1][1][d['name']]['v'] != sequence[m-2][1][d['name']]['v']: # we need to keep voltage
-#                    dv = 0
-#                    t = 0
-#                    n = 0
-#                    while sequence[m+n][1][d['name']]['v'] == sequence[m+n-1][1][d['name']]['v']:
-#                        t += sequence[m+n][0]
-#                        if m + n < len(sequence)-1:
-#                            n += 1
-#                        else:
-#                            break
-#                    ba += [int(eval(hex(self.ramp_rate(dv, t))) >> i & 0xff) for i in range(0, 16, 8)]
-#                    ba += [int(eval(hex(self.time_to_ticks(t))) >> i & 0xff) for i in range(0, 32, 8)]
-#        ba += [0]*12
-#	print 'writing ', len(ba), ' bytes...'
-#        return ba
-    
-    def _make_sequence(self, sequence):
+    def make_sequence(self, board, sequence):
         """ sequence is list of tuples [(duration, {name: logic}), ...] """
         # remove unnecessary ramps -> get all the ramp rates. if consequtive ramprates are same, combine!!!
-        # write necessary ramps in correct order. Q
+        # write necessary ramps in correct order. 
+
+        # peel away unnecessary channels
+        for s in sequence:
+            d = s[1]
+            peeled_cd = {c.key: d[c.key] for c in board.channels.values()}
+
+        # make list of linear ramps [(time, {id, dt, dv})]
+
+
+
+
+
         ba = []
-	pts = 0
-        for k, d in sorted(self.channels.items()):
-            dv = sequence[0][1][d['name']]['v'] 
+        pts = 0
+        for k, c in sorted(board.channels.items()):
+            dv = sequence[0][1][c.name]['v'] 
             if dv == 0:
                 t = sequence[0][0]
                 n = 0
-                while sequence[n+1][1][d['name']]['v'] == sequence[n][1][d['name']]['v']:
+                while sequence[n+1][1][c.key]['v'] == sequence[n][1][c.key]['v']:
                     t += sequence[n+1][0]
                     if n < len(sequence)-2:
                         n += 1
@@ -179,18 +158,18 @@ class SequencerServer(LabradServer):
         for m in range(1, len(sequence)):
             ba_buffer = {}
             for k, d in sorted(self.channels.items()): # add counter l for multiple linear ramps
-                if float(sequence[m][1][d['name']]['v']) != float(sequence[m-1][1][d['name']]['v']): # we need to change voltage
-                    if sequence[m][1][d['name']]['type'] == 'linear':
-                        dv = sequence[m][1][d['name']]['v'] - sequence[m-1][1][d['name']]['v']
+                if float(sequence[m][1][c.key]['v']) != float(sequence[m-1][1][c.key]['v']): # we need to change voltage
+                    if sequence[m][1][c.key]['type'] == 'linear':
+                        dv = sequence[m][1][c.key]['v'] - sequence[m-1][1][c.key]['v']
                         t = sequence[m][0]
                         ba += [int(eval(hex(self.ramp_rate(dv, t))) >> i & 0xff) for i in range(0, 16, 8)]
                         ba += [int(eval(hex(self.time_to_ticks(t))) >> i & 0xff) for i in range(0, 32, 8)]
-                    elif sequence[m][1][d['name']]['type'] == 'exp': 
-                        vi = float(sequence[m-1][1][d['name']]['v'])
-			vf = float(sequence[m][1][d['name']]['v'])
+                    elif sequence[m][1][c.key]['type'] == 'exp': 
+                        vi = float(sequence[m-1][1][c.key]['v'])
+			vf = float(sequence[m][1][c.key]['v'])
 			t_tot = float(sequence[m][0])
-			pts = int(sequence[m][1][d['name']]['pts'])
-			tau = float(sequence[m][1][d['name']]['tau'])
+			pts = int(sequence[m][1][c.key]['pts'])
+			tau = float(sequence[m][1][c.key]['tau'])
 			a = (vf - vi) / (np.exp(-t_tot/tau) - 1)
 			c = vi - a
 			continuous = lambda t: a*np.exp(-t/tau) + c
@@ -202,7 +181,7 @@ class SequencerServer(LabradServer):
 			print 'dVs', dV
 			ba += [int(eval(hex(self.ramp_rate(dV[0], dT[0]))) >> i & 0xff) for i in range(0, 16, 8)]
 			ba += [int(eval(hex(self.time_to_ticks(dT[0]))) >> i & 0xff) for i in range(0, 32, 8)]
-			ba_buffer[d['name']] = []
+			ba_buffer[c.key] = []
 
                         total_ticks = self.time_to_ticks(t_tot)
 			dT_ticks = [self.time_to_ticks(dt) for dt in dT]
@@ -210,13 +189,13 @@ class SequencerServer(LabradServer):
 			print 'len', len(dV)
 
 			for dt, dt_t, dv in zip(dT[1:], dT_ticks[1:], dV[1:]):
-                            ba_buffer[d['name']] += [int(eval(hex(self.ramp_rate(dv, dt))) >> i & 0xff) for i in range(0, 16, 8)]
-                            ba_buffer[d['name']] += [int(eval(hex(dt_t)) >> i & 0xff) for i in range(0, 32, 8)]
-                elif sequence[m-1][1][d['name']]['v'] != sequence[m-2][1][d['name']]['v']: # we need to keep voltage
+                            ba_buffer[c.key] += [int(eval(hex(self.ramp_rate(dv, dt))) >> i & 0xff) for i in range(0, 16, 8)]
+                            ba_buffer[c.key] += [int(eval(hex(dt_t)) >> i & 0xff) for i in range(0, 32, 8)]
+                elif sequence[m-1][1][c.key]['v'] != sequence[m-2][1][c.key]['v']: # we need to keep voltage
                     dv = 0
                     t = 0
                     n = 0
-                    while sequence[m+n][1][d['name']]['v'] == sequence[m+n-1][1][d['name']]['v']:
+                    while sequence[m+n][1][c.key]['v'] == sequence[m+n-1][1][c.key]['v']:
                         t += sequence[m+n][0]
                         if m + n < len(sequence)-1:
                             n += 1
@@ -224,101 +203,98 @@ class SequencerServer(LabradServer):
                             break
                     ba += [int(eval(hex(self.ramp_rate(dv, t))) >> i & 0xff) for i in range(0, 16, 8)]
                     ba += [int(eval(hex(self.time_to_ticks(t))) >> i & 0xff) for i in range(0, 32, 8)]
-	    cont = True
 	    for i in range(pts-1):
 	    	for k in sorted(ba_buffer.keys()):
                     ba += ba_buffer[k][6*i:6*(i+1)]
 		    print k, ba_buffer[k][6*i:6*(i+1)]
         ba += [0]*24
-	print 'writing ', len(ba), ' bytes...'
+        print 'writing ', len(ba), ' bytes...'
         return ba
     
-    def _program_sequence(self, sequence):
-        ba = self._make_sequence(sequence)
-        self.set_sequencer_mode('idle')
-        self.set_sequencer_mode('load')
-        self.xem.WriteToPipeIn(0x80, bytearray(ba))
-        self.set_sequencer_mode('idle')
+    def program_sequence(self, sequence):
+        for board in self.boards.values():
+            ba = self.make_sequence(board, sequence)
+            self.set_board_mode(board, 'idle')
+            self.set_board_mode(board, 'load')
+            board.xem.WriteToPipeIn(0x80, bytearray(ba))
+            self.set_board_mode(board, 'idle')
     
-    def set_sequencer_mode(self, mode):
-        self.xem.SetWireInValue(0x00, self.sequencer_mode_num[mode])
-        self.xem.UpdateWireIns()
-        self.seuqencer_mode = mode
+    def set_board_mode(self, board, mode):
+        board.xem.SetWireInValue(board.mode_wire, board.mode_nums[mode])
+        board.xem.UpdateWireIns()
+        board.mode = mode
 
     @setting(01, 'get channels')
     def get_channels(self, c):
-        return str({k: d['name'] for k, d in self.channels.items()})
+        channels = {}
+        for b in self.boards.values():
+            for k, c in b.channels.items():
+                channels[k] = c.__dict__
+        return str(channels)
 
-    @setting(07, 'run sequence', sequence='s')
+    @setting(07, 'program sequence', sequence='s')
     def run_sequence(self, c, sequence):
         sequence = json.loads(sequence)
-        self._program_sequence(sequence)
-        self.set_sequencer_mode('run')
-    
-    @setting(02, 'run sequence from file', file_name='s')
-    def run_sequence_from_file(self, c, file_name):
-        infile = open(file_name, 'r')
-        sequence = [eval(line.split('\n')[:-1][0]) for line in infile.readlines()]
-        self._program_sequence(sequence)
-        self.set_sequencer_mode('run')
-        print '!!!'
-        return file_name
+        self.program_sequence(sequence)
+        for board in self.boards.values():
+            self.set_board_mode(board, 'run')
 
     @setting(03, 'sequencer mode', mode='s')
-    def _sequencer_mode(self, c, mode=None):
+    def sequencer_mode(self, c, mode=None):
         if mode is not None:
-            self.set_sequencer_mode(mode)
-        return self.sequencer_mode
+            for board in self.boards.values():
+                self.set_board_mode(board, mode)
+            self.mode = mode
+        return self.mode
 
-    def write_channel_modes(self): 
-        cm_list = [d['mode'] for k, d in sorted(self.channels.items())]
-        ba = sum([2**j for j, m in enumerate(cm_list) if m == 'manual'])
-        self.xem.SetWireInValue(self.channel_mode_wire, ba)
-        self.xem.UpdateWireIns()
-
-    @setting(04, 'channel mode', channel='s', mode='s')
-    def channel_mode(self, c, channel, mode=None):
+    @setting(04, 'channel mode', channel_id='s', mode='s')
+    def channel_mode(self, c, channel_id, mode=None):
+        channel = self.id2channel(channel_id)
+        if not channel:
+            raise ChannelNotFound()
         if mode is not None:
-            self.channels[self.name_to_key[channel]]['mode'] = mode
-            self.write_channel_modes()
+            channel.mode = mode
+            board = self.boards[channel.loc[0]]
+            self.write_channel_modes(board)
         self.notify_listeners(c)
-        return self.channels[self.name_to_key[channel]]['mode']
+        return channel.mode
     
-    def write_channel_manual_voltage(self, channel): 
-        key = self.name_to_key[channel]
-        voltage = self.voltage_to_unsigned(self.channels[key]['manual voltage'])
-        wire = self.manual_voltage_wires[sorted(self.channels).index(key)]
-        self.xem.SetWireInValue(wire, voltage)
-        self.xem.UpdateWireIns()
-
-    @setting(05, 'channel manual voltage', channel='s', voltage='v')
-    def channel_manual_voltage(self, c, channel, voltage=None):
+    def write_channel_modes(self, board): 
+        cm_list = [c.mode for k, c in sorted(board.channels.items())]
+        mode_value = sum([2**j for j, m in enumerate(cm_list) if m == 'manual'])
+        board.xem.SetWireInValue(board.channel_mode_wire, mode_value)
+        board.xem.UpdateWireIns()
+    
+    @setting(05, 'channel manual voltage', channel_id='s', voltage='v')
+    def channel_manual_voltage(self, c, channel_id, voltage=None):
+        channel = self.id2channel(channel_id)
+        if not channel:
+            raise ChannelNotFound()
         if voltage is not None:
             voltage = sorted([-10, voltage, 10])[1]
-            self.channels[self.name_to_key[channel]]['manual voltage'] = voltage
-            self.write_channel_manual_voltage(channel)
+            channel.manual_voltage = voltage
+            self.write_manual_voltage(channel)
         self.notify_listeners(c)
-        return self.channels[self.name_to_key[channel]]['manual voltage']
+        return channel.manual_voltage
 
-    @setting(06, 'get channel configuration', channel='s', returns='s')
-    def get_channel_configuration(self, c, channel):
-        yield None
-        key = self.name_to_key(channel)
-        returnValue(json.dumps(self.channels[key]))
+    def write_manual_voltage(self, channel): 
+        board = self.boards[channel.loc[0]]
+        wire = board.manual_voltage_wires[channel.loc[1:]]
+        voltage = self.voltage_to_unsigned(channel.manual_voltage)
+        board.xem.SetWireInValue(wire, voltage)
+        board.xem.UpdateWireIns()
+
+    @setting(06, 'get channel configuration', channel_id='s')
+    def get_channel_configuration(self, c, channel_id):
+        channel = self.id2channel(channel_id)
+        return json.dumps(channel.__dict__)
 
     @setting(10, 'notify listeners')
     def notify_listeners(self, c):
         self.update(json.dumps(self.channels))
 
-#    @setting(06, 'channel invert', channel='s', invert='i')
-#    def channel_invert(self, c, channel, invert=None):
-#        if invert is not None:
-#            self.channels[self.name_to_key[channel]]['invert'] = invert
-#            self.write_channel_stateinvs()
-#        return self.channels[self.name_to_key[channel]]['invert']
-
 if __name__ == "__main__":
     config_name = 'analog_config'
-    __server__ = SequencerServer(config_name)
+    __server__ = AnalogSequencerServer(config_name)
     from labrad import util
     util.runServer(__server__)
