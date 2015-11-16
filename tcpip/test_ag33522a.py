70cdec89d0c05e6649bb571db3fd11a2d714ef39
ross hutson <rosshutson@gmail.com>
2015-11-16 14:02:48 -0700

    .

--- tcpip/test_ag33522a.py
+++ tcpip/test_ag33522a.py
@@ -4,6 +4,9 @@ import time
 
 import labrad
 
+SAMPLE_RATE = 100e3 # [Hz]
+MODULATION_FREQUENCY = 30e3 # [Hz]
+
 waveform = {
             'channel name': 'alpha fm', 
             'name': 'test', 
@@ -11,6 +14,11 @@ waveform = {
             'points': np.linspace(0, 1, 1e4).tolist(),
            }
 
+times = [('T_cap', 50e-3), ('T_bb', 400e-3), ('T_sf', 200e-3), ('T_load', 50e-3)]
+
+
+T = np.arange(0, sum([t for n, t in times]), 1/SAMPLE_RATE)
+
 cxn = labrad.connect()
 awg = cxn.AG33522A
-awg.program_waveform(json.dumps(waveform))
+#awg.program_waveform(json.dumps(waveform))
