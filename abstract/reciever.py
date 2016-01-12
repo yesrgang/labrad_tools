ac2dcfe975f4d9c7caabe845ef1265647ebf4640
yesrgang <yesrgang@gmail.com>
2016-01-12 15:54:07 -0700

    .

--- abstract/reciever.py
+++ abstract/reciever.py
@@ -1,8 +1,8 @@
 import json
 import os
+import time
 
 import ok
-from time import sleep
 from labrad.server import LabradServer, setting, Signal
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks, returnValue, DeferredLock
@@ -32,16 +32,43 @@ class ReceiverServer(LabradServer):
 	    if len(self.data) > 1:
                 print 'appending {}/{}'.format(len(self.data)-1, self.num_cycles)
             if len(self.data) >= self.num_cycles+1:
-                iteration = 2
-                while os.path.isfile(self.filename):
-                    name = self.filename.split('#')[0]
-    		    self.filename = name + '#{}'.format(iteration)
-		    iteration += 1
-                with open(self.filename, 'w+') as outfile:
-			json.dump(self.data[1:], outfile)
-                print 'done recording {}'.format(self.filename)
-    		self.data = []
+#                iteration = 2
+#                while os.path.isfile(self.filename):
+#                    name = self.filename.split('#')[0]
+#    		    self.filename = name + '#{}'.format(iteration)
+#		    iteration += 1
+#                with open(self.filename, 'w+') as outfile:
+#			json.dump(self.data[1:], outfile)
+#                print 'done recording {}'.format(self.filename)
+#    		self.data = []
+                self._write()
     		self.do_record = False
+
+    def _write(self):
+        # write file to todays data folder
+	data_dir = 'Z:\\SrQ\\data\\'
+	todays_dir = data_dir + time.strftime('%Y%m%d')
+	if not os.path.exists(todays_dir):
+            os.mkdir(todays_dir)
+        
+	# append experiment run number
+        filename = todays_dir + '\\' + self.filename + '#0'
+	iteration = 0
+        while os.path.isfile(filename):
+            filename = filename.split('#')[0] + '#{}'.format(iteration)
+	    iteration += 1
+
+        # data [{}] -> {[]}
+
+        with open(filename, 'w+') as outfile:
+		json.dump(self.data[1:], outfile)
+    	self.data = []
+	try:
+            self.clinet.yesr9_beeper.beep()
+	except:
+            pass
+        print 'data written to {}'.format(filename)
+        
 
     @setting(2, 'record', filename='s', num_cycles='i')
     def record(self, c, filename, num_cycles):
@@ -51,9 +78,11 @@ class ReceiverServer(LabradServer):
 	self.num_cycles = num_cycles
 	self.do_record = True
 
-    @setting(3, 'stop record')
-    def stop_record(self, c):
+    @setting(3, 'stop record', do_write='b')
+    def stop_record(self, c, do_write=True):
         self.do_record = False
+	if do_write:
+            self._write()
 
 if __name__ == "__main__":
     __server__ = ReceiverServer()
