f79aaa67d5045f81612b1df8d190e893b58a93e6
ross hutson <rosshutson@gmail.com>
2016-01-29 18:32:27 -0700

    .

--- scripts/scan_clock.py
+++ scripts/scan_clock.py
@@ -4,32 +4,34 @@ import numpy as np
 
 filename = 'scan_clock'
 
-c_freq = 27.05e6
-f_range = 100e3
-f_step = 1e3
+c_freq = 27.134e6 - 34e3
+f_range = 3e3
+f_step = .2e3
 
 freqs = np.arange(c_freq-f_range, c_freq + f_range, f_step)
-
-f_start = 27.1173e6
-f_range = 220e3
-f_step = .1e3
-f_stop = 27.1185e6
-freqs = np.arange(f_start, f_stop, f_step)
+#
+#f_start = 27.1173e6
+#f_range = 220e3
+#f_step = .1e3
+#f_stop = 27.1185e6
+#freqs = np.arange(f_start, f_stop, f_step)
 
 device_parameters = {
     'Clock AOM': {
         'frequency': {
             'init command': "self.client._33500b.select_device_by_name('clock steer')", 
             'command': "lambda value, self=self: self.client._33500b.frequency(value)",
-            'value': 27.1179e6 #freqs.tolist(),
+#            'value': c_freq
+            'value': freqs.tolist(),
             },
         },
     }
 
 sequence_parameters = {
-    '*813H1f': -2,
-    '*813H2f': -2,
-    '*Iclk': -.09,
+    '*813H1f': -.4,
+    '*813H2f': -.4,
+    '*813Vf': -3,
+    '*Iclk': -.03,
 }
 
 cxn = labrad.connect()
@@ -37,4 +39,4 @@ c = cxn.yesr20_conductor
 r = cxn.yesr20_receiver
 c.set_device_parameters(json.dumps(device_parameters))
 c.update_sequence_parameters(json.dumps(sequence_parameters))
-#r.record(filename, len(device_parameters['Clock AOM']['frequency']['value']))
+r.record(filename, len(device_parameters['Clock AOM']['frequency']['value']))
--- scripts/scan_exap_rampvodt.py
+++ scripts/scan_exap_rampvodt.py
@@ -3,10 +3,10 @@ import json
 import numpy as np
 
 filename = 'evap'
-vi_1 = -1.2
+vi_1 = -1.0
 vf_1 = -.3
-T_1 = 10
-tau_1 = -5
+T_1 = 5
+tau_1 = -2.5
 a = (vf_1-vi_1)/(np.exp(T_1/tau_1) - 1)
 c = vi_1 - a
 
@@ -14,9 +14,9 @@ v_1 = np.linspace(vi_1, vf_1, 10)
 T_l1 = tau_1 * np.log((v_1 - c)/a)
 
 vi_2 = v_1[-6]
-vf_2 = -.13
-T_2 = 20.
-tau_2 = -10
+vf_2 = -.16
+T_2 = 10
+tau_2 = -5
 a = (vf_2-vi_2)/(np.exp(T_2/tau_2) - 1)
 c = vi_2 - a
 
@@ -27,10 +27,10 @@ print T_l2
 
 
 vi_vert = -.2
-vf_vert = -.2
+vf_vert = -.5
 v_vert = np.linspace(vi_vert, vf_vert, 10)
 
-take = 0
+take = -1
 
 scan_parameters = {
     '*HODTi': vi_1,
@@ -40,6 +40,7 @@ scan_parameters = {
     '*Tevap': T_l2[1:].tolist()[take],
     '*tau_1': tau_1,
     '*tau_2': tau_2,
+    '*tau_3': -tau_2,
     '*VODTi': vi_vert,
     '*VODTf': v_vert[1:].tolist()[take], 
     'description': 'watch evaporation, new VODT waist (~60um)'
