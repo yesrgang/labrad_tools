00f5901ace2dad94ccedf9e18e39a77d0ba67b4f
yesrgang <ross.hutson@jila.colorado.edu>
2016-06-28 21:21:27 -0600

    Update scan_rabi_flop.py

--- example/scripts/scan_rabi_flop.py
+++ example/scripts/scan_rabi_flop.py
@@ -2,17 +2,14 @@ from experiments import *
 
 name = 'scan_rabi_flop'
 
-c_freq = 27.326209e6
-shift_freq = 2.3667e3
 clock_intensity = -5
 
-c_freq = c_freq-shift_freq
-t_start = .001
+t_start = 0.0
 t_stop = 0.8
-n_point = 20
+n_points = 20
 
 """ should not regularly need to change stuff below here """
-times = np.linspace(t_start, t_stop, n_point).tolist()
+times = np.linspace(t_start, t_stop, n_points).tolist()[1:]
 parameters = {
     'sequence': {
         '*Trabi': times,
