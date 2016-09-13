4212892367b02e22fac44a70f6e23870b1167823
yesrgang <yesrgang@gmail.com>
2016-09-13 00:53:23 -0600

    .

--- conductor/config.py
+++ conductor/config.py
@@ -2,12 +2,7 @@ import time
 
 class ConductorConfig(object):
     def __init__(self):
-        self.update_id = '689989'
-        self.db_query_str = 'SELECT value FROM "experiment parameters" WHERE \
-                "device" = \'sequence\' AND "parameter" = \'{}\' ORDER BY time \
-                DESC LIMIT 1'
         self.data_directory = lambda: 'Z:\\SrQ\\data\\' + time.strftime('%Y%m%d') + '\\'
-        
         self.default_parameters = {
                 'clock_aom': {
                     'frequency': None,
