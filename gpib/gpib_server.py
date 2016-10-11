0f88fbfacb9ec4b9947fad960090ed7a819da7e2
ross hutson <rosshutson@gmail.com>
2016-10-11 15:52:34 -0600

    .

--- gpib/gpib_server.py
+++ gpib/gpib_server.py
@@ -22,15 +22,15 @@ from labrad.server import LabradServer, setting
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.reactor import callLater
 
-from server_tools.hardware_communication_server import HardwareCommunicationServer
+from server_tools.hardware_interface_server import HardwareInterfaceServer
 
 
 class GPIBServer(HardwareCommunicationServer):
     """Provides direct access to GPIB-enabled hardware."""
     name = '%LABRADNODE%_gpib'
 
-    def refresh_available_hardware(self):
-        """ fill self.hardware with available connections """
+    def refresh_available_interfaces(self):
+        """ fill self.interfaces with available connections """
         rm = visa.ResourceManager()
         addresses = rm.list_resources()
         additions = set(addresses) - set(self.devices.keys())
@@ -40,17 +40,16 @@ class GPIBServer(HardwareCommunicationServer):
                 inst = rm.get_instrument(address)
                 inst.write_termination = ''
                 inst.clear()
-                self.hardware[address] = inst
+                self.interfaces[address] = inst
                 print 'connected to GPIB device ' + address
         for addr in deletions:
             del self.devices[addr]
-            self.sendDeviceMessage('GPIB Device Disconnect', addr)
 
     @setting(3, data='s', returns='')
     def write(self, c, data):
         """Write a string to the GPIB bus."""
         connection = self.get_connection(c)
-        connection.write(data)
+        self.call_if_available('write', c, data)
 
     @setting(4, n_bytes='w', returns='s')
     def read(self, c, n_bytes=None):
@@ -59,8 +58,7 @@ class GPIBServer(HardwareCommunicationServer):
         If specified, reads only the given number of bytes.
         Otherwise, reads until the device stops sending.
         """
-        connection = self.get_connection(c)
-        ans = connection.read_raw()
+        ans = self.call_if_available('read_raw', c)
         return str(ans).strip()
 
     @setting(5, data='s', returns='s')
@@ -71,8 +69,8 @@ class GPIBServer(HardwareCommunicationServer):
         device will occur while the query is in progress.
         """
         connection = self.get_connection(c)
-        connection.write(data)
-        ans = connection.read_raw()
+        self.call_if_available('write', data)
+        ans = self.call_if_available('read_raw')
         return str(ans).strip()
 
 if __name__ == '__main__':
