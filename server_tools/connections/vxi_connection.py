771aa6810d7975979a71aa173c0ea23a40dd9f8f
srgang <yesrgang@gmail.com>
2017-04-25 22:00:00 -0600

    adding vxi server for private network

--- server_tools/connections/vxi_connection.py
+++ server_tools/connections/vxi_connection.py
@@ -2,17 +2,47 @@ from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.threads import deferToThread
 from vxi11 import Instrument
 
-class VXIConnection(object):
+LABRADHOST = os.getenv('LABRADHOST')
+
+class GPIBConnection(object):
     @inlineCallbacks
     def initialize(self, device):
-        yield None
-        self.connection = Instrument(device.address)
+        self.connection = yield connectAsync(LABRADHOST)
+        self.server = self.connection[device.servername]
+        yield self.server.select_interface(device.address)
 
-    @inlineCallbacks 
-    def write(self, value):
-        yield deferToThread(self.connection.write, value)
+    @inlineCallbacks
+    def write(self, data):
+        yield self.server.write(data)
     
-    @inlineCallbacks 
-    def ask(self, value):
-        ans = yield deferToThread(self.connection.ask, value)
+    @inlineCallbacks
+    def read(self):
+        ans = yield self.server.read()
         returnValue(ans)
+    
+    @inlineCallbacks
+    def query(self, data):
+        ans = yield self.server.query(data)
+        returnValue(ans)
+
+    @inlineCallbacks
+    def timeout(self, timeout):
+        ans = yield self.server.timeout(timeout)
+        returnValue(ans)
+
+#class VXIConnection(object):
+#    @inlineCallbacks
+#    def initialize(self, device):
+#        yield None
+#        self.connection = Instrument(device.address)
+#
+#    @inlineCallbacks 
+#    def write(self, value):
+#        yield deferToThread(self.connection.write, value)
+#    
+#    @inlineCallbacks 
+#    def ask(self, value):
+#        ans = yield deferToThread(self.connection.ask, value)
+#        returnValue(ans)
+#
+
